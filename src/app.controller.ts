369e33ffa9411f65cb437e022378ead845c2f21d
AlexanderJumbo <geossephyjumboalava@gmail.com>
2026-01-11 12:51:01 -0500

    Envío de notificaciones desde el back

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Post } from '@nestjs/common';
+// import { AppService } from './app.service';
+import { PushNotificationsService } from './push-notifications/push-notifications.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  // constructor(private readonly appService: AppService) { }
+  constructor(
+    private readonly pushNotificationService: PushNotificationsService,
+  ) { }
 
-  @Get()
-  getHello() {
-    return this.appService.getHello();
+  // @Get()
+  // getHello() {
+  //   return this.appService.getHello();
+  // }
+
+  @Post('/send-notification')
+  sendNotification(@Body() body: { to: string[] }) {
+    // const toTokens = ['ExponentPushToken[MmTBFTPm65DtCok11yzQrX]'];
+
+    // this.pushNotificationService.sendNotification(toTokens);
+    this.pushNotificationService.sendNotification(body.to);
   }
 }
